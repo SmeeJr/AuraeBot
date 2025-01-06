eba5fe7825e1b013ff480d22d551afa945708b99
SmeeJr <137827522+SmeeJr@users.noreply.github.com>
2025-01-06 02:34:25 +0100

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     bot: {
-        prefixe: '+', // Préfixe - Le texte avant les commandes
+        prefixe: '-', // Préfixe - Le texte avant les commandes
         buyer: '1122190536577646704', // Buyer
         couleur: '#6495ED', // Couleur HEX
         footer: 'AuraeBot', // Dans la commande help ou autre il y'a un texte en bas
