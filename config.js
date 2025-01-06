4e7f876d4c4b35c1fc95443d630f23c9e5207208
SmeeJr <137827522+SmeeJr@users.noreply.github.com>
2025-01-06 02:22:29 +0100

    config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1122190536577646704', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
+        footer: 'AuraeBot', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
